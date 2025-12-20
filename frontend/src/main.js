9a3f357fb948b9cb1a531205b2116902b5b6584d
Princekrcoder <princekrcoder@gmail.com>
2025-12-21 00:36:25 +0530

    Auto commit: 21-12-2025  0:36:25.19

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,8 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/styles/main.css'
+import './assets/styles/navbar.css'
+import './assets/styles/footer.css'
 
 const app = createApp(App)
 
