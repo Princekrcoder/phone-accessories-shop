65ca3a35b809a9a95e2916b9b8eb7ee368ada808
Princekrcoder <princekrcoder@gmail.com>
2025-12-21 12:04:37 +0530

    Auto commit: 21-12-2025 12:04:37.28

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -78,7 +78,7 @@
       </div>
 
       <div id="logo-row">
-        <img v-for="(logo)" :src="logo" alt="logo">
+        <img v-for="(logo, index) in logos" :src="logo" alt="logo">
       </div>
 
     </div>
