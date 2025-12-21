dfea606baafb9fbed07472bb3029b2c228d17086
Princekrcoder <princekrcoder@gmail.com>
2025-12-21 12:38:23 +0530

    Auto commit: 21-12-2025 12:38:23.28

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,13 +2,13 @@
   import Header from "./components/Header.vue";
   import AppFooter from "./components/AppFooter.vue";
   import Home from "./views/Home.vue";
-  import Navbar from "./components/Navbar.vue";
+  
 </script>
 
 <template>
   <div id="app">
     <Header />
-    <Navbar />
+    
     <Home />
     <AppFooter />
     
