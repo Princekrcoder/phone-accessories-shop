9cf03588bf2d489ea2ae4f9de1c2514841b55518
Princekrcoder <princekrcoder@gmail.com>
2025-12-21 12:18:01 +0530

    Auto commit: 21-12-2025 12:18:01.23

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -88,7 +88,7 @@
 
 <script setup>
 import logo1 from "@/assets/logo/logo1.png";
-import logo1 from "@/assets/logo/logo1.png";
+
 import logo2 from "@/assets/logo/logo2.png";
 import logo3 from "@/assets/logo/logo3.png";
 import logo4 from "@/assets/logo/logo4.png";
