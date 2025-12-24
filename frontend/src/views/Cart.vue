9c94519691c6260bd5b524d750683cef28b58b7c
Princekrcoder <princekrcoder@gmail.com>
2025-12-24 21:17:39 +0530

    Auto commit: 24-12-2025 21:17:39.24

--- frontend/src/views/Cart.vue
+++ frontend/src/views/Cart.vue
@@ -75,7 +75,7 @@
           <span>₹29.00</span>
         </div>
 
-        <router-link to="/" class="continue-btn">← CONTINUE SHOPPING</router-link>
+        <router-link to="/" class="continue-btn " type="button" class="btn btn-primary btn-sm">← CONTINUE SHOPPING</router-link>
       </div>
 
       <!-- RIGHT: Cart Total -->
