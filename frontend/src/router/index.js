6ccb65cb2225199a7fede8b28d28b5f186bc2b89
Princekrcoder <princekrcoder@gmail.com>
2025-12-24 19:59:27 +0530

    Auto commit: 24-12-2025 19:59:27.28

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import
+import Cart from '@/views/Cart.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
