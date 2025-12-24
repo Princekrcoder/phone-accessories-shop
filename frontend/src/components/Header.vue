664d8337b6e1019a24bcaa043d8c669babf2e9e6
Princekrcoder <princekrcoder@gmail.com>
2025-12-24 19:51:35 +0530

    Auto commit: 24-12-2025 19:51:35.21

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -44,7 +44,7 @@
         </div>
 
         <button class="user">
-            <i class="bi bi-person-circle" style="pa font-size: 36px;"></i>
+            <i class="bi bi-person-circle" style=" font-size: 36px;"></i>
         </button>
     </header>
 
