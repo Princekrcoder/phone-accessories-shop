c404b1dcc64ed41b6d266b9cbe0a600328336718
Princekrcoder <princekrcoder@gmail.com>
2025-12-24 15:26:18 +0530

    Auto commit: 24-12-2025 15:26:18.24

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -49,7 +49,7 @@ onMounted(() => {
         <ol class="nav-links">
             <li href="#">MOBILE COVER</li>
             <li href="#">MORE</li>
-            <a href="#">OFFER <span class="badge">NEW</span></a>
+            <li href="#">OFFER <span class="badge">NEW</span></li>
         </ol>
 
         <!-- SEARCH + CART -->
