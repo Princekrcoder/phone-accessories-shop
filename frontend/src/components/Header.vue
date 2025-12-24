22d1fde865f25ea0bd90e5078306619ca3dc7d74
Princekrcoder <princekrcoder@gmail.com>
2025-12-24 20:40:41 +0530

    Auto commit: 24-12-2025 20:40:41.44

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -40,7 +40,7 @@ import { RouterLink } from 'vue-router';
                 <input type="text" placeholder="Search..." />
                 <span class="search-icon">🔍</span>
             </div>
-            <router-link to="/Cart" class="cart-btn">
+            <router-link to="/cart" class="cart-btn">
                 ₹2300000.00<i class="bi bi-bag-fill" style="padding-left: 3px;"></i>
             </router-link>
         </div>
