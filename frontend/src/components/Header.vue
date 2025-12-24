8be3062604ab0a43fb176cf94473e4d61311b423
Princekrcoder <princekrcoder@gmail.com>
2025-12-24 19:52:39 +0530

    Auto commit: 24-12-2025 19:52:39.29

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -39,7 +39,7 @@
                 <span class="search-icon">🔍</span>
             </div>
             <button class="cart-btn">
-                <p style="ba">₹23.00</p><i class="bi bi-bag-fill"></i>
+                <p style="padding-right: 5px;">₹23.00</p><i class="bi bi-bag-fill"></i>
             </button>
         </div>
 
