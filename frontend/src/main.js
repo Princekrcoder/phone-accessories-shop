1fdf6e9cda7ff4b2da03661de0c6ca62464281b9
Princekrcoder <princekrcoder@gmail.com>
2025-12-24 16:30:55 +0530

    Auto commit: 24-12-2025 16:30:55.19

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,6 +8,10 @@ import './assets/styles/main.css'
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 import "./fontawesome"
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 
 const app = createApp(App)
 
