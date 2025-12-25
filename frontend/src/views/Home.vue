482195e4e1b3e590f888d37839fda39b8dc525b4
Princekrcoder <princekrcoder@gmail.com>
2025-12-25 18:22:22 +0530

    Auto commit: 25-12-2025 18:22:22.27

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,23 +4,7 @@
     <p>Best accessories at best price 🔥</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, itaque ipsam? Recusandae possimus, asperiores, laboriosam doloremque voluptate similique, dolorem quasi quidem libero quos placeat illum distinctio iste fugiat. Modi aspernatur consectetur explicabo. Tenetur dignissimos ab nostrum porro totam praesentium ullam aliquid, inventore accusantium distinctio rerum magni rem eius, eos eaque optio saepe nihil officiis. Eveniet iusto, ut odit quis necessitatibus saepe a! Eligendi ea voluptates inventore non expedita qui molestiae illo ullam officia omnis, iure repudiandae neque adipisci vitae voluptas, nobis temporibus ipsum nemo quaerat iusto ducimus voluptate in sed? Beatae perferendis laboriosam earum cupiditate aspernatur autem voluptates quam vitae?</p>
     
-    <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-
-<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+    sect
 
   </div>
 </template>
