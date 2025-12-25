e25e3383434d93e1e9328af423968429df1d97f6
Princekrcoder <princekrcoder@gmail.com>
2025-12-25 18:23:26 +0530

    Auto commit: 25-12-2025 18:23:26.22

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,7 +4,7 @@
     <p>Best accessories at best price 🔥</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, itaque ipsam? Recusandae possimus, asperiores, laboriosam doloremque voluptate similique, dolorem quasi quidem libero quos placeat illum distinctio iste fugiat. Modi aspernatur consectetur explicabo. Tenetur dignissimos ab nostrum porro totam praesentium ullam aliquid, inventore accusantium distinctio rerum magni rem eius, eos eaque optio saepe nihil officiis. Eveniet iusto, ut odit quis necessitatibus saepe a! Eligendi ea voluptates inventore non expedita qui molestiae illo ullam officia omnis, iure repudiandae neque adipisci vitae voluptas, nobis temporibus ipsum nemo quaerat iusto ducimus voluptate in sed? Beatae perferendis laboriosam earum cupiditate aspernatur autem voluptates quam vitae?</p>
     
-    <section class="">
+    <section class="item-list">
       <div class="card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
