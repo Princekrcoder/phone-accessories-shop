00499120ab98271808213f0e3b5f5c2a7cd4618f
Princekrcoder <princekrcoder@gmail.com>
2025-12-25 16:48:24 +0530

    Auto commit: 25-12-2025 16:48:24.19

--- frontend/src/views/Category.vue
+++ frontend/src/views/Category.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="category-page">
         <!-- Top Button -->
-         
+         <div id="top-button">4
+            <button>E</button>
+         </div>
     </div>
 </template>
