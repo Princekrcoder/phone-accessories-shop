c197613b221a4f576d80669c553810f2319b3d8c
Princekrcoder <your-email@example.com>
2025-12-29 18:25:38 +0530

    Auto commit: 29-12-2025 18:25:38.35

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import Cart from '@/views/Cart.vue'
 import Home from '@/views/Home.vue'
 import CategoryPage from '@/views/CategoryPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+
   routes: [
     {
       path: '/cart',
