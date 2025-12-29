cb89cbb6ded32a4ea2756862080e76058e18a9eb
Princekrcoder <your-email@example.com>
2025-12-29 07:48:42 +0530

    Auto commit: 29-12-2025  7:48:42.24

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -6,7 +6,7 @@
         <div class="popular-searches">
             <span class="section-title">
                 <strong>POPULAR SEARCHES</strong>
-                <img src="@/assets/image/underline.png" alt="">
+                <img class="underline-img" src="@/assets/image/underline.png" alt="">
             </span>
 
             <p class="dis">
