2305630c1c8f3f2245f99819f5822b1d1575767d
Princekrcoder <your-email@example.com>
2025-12-29 09:18:23 +0530

    Auto commit: 29-12-2025  9:18:23.18

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -54,6 +54,7 @@ import "@/assets/styles/item.css";
 import { name } from "@vue/eslint-config-prettier/skip-formatting";
 
 const categories = [
-    {name: "Mobile Covers", slug: "mobile-covers"},
+    {name: "Mobile Covers", slug: "mobile-covers", products: 12},
+    
 ] 
 </script>
